6af538df3043cd57a718abad3edd76d19e96e6ad
jesus-molano <jessumolano@gmail.com>
2022-04-24 15:17:39 +0100

    Added base to vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:"/",
+  base:"/vue3-manz-tuto/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
